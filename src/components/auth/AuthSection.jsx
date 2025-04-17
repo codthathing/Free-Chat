f945fc86e49fedb41b53e13b76181a156d34c3ea
Uniquecon <akinwunmiolusegun277@gmail.com>
2025-04-17 14:08:20 +0100

    feat: completed the updated modal

--- src/components/auth/AuthSection.jsx
+++ src/components/auth/AuthSection.jsx
@@ -1,6 +1,6 @@
 const AuthSection = ({ children }) => {
   return (
-    <section className="h-screen md:flex md:justify-center md:items-center md:bg-gray-light">
+    <section className="h-full md:h-screen md:flex md:justify-center md:items-center md:bg-gray-light">
       <main className={`h-full md:h-fit md:w-3/5 lg:w-2/6 bg-white md:rounded-3xl lg:rounded-2xl flex flex-col px-4 mt-8 md:mt-0 md:p-8 lg:p-6 gap-y-5 md:gap-y-8 lg:gap-y-7`}>
         {children}  
       </main>
--- src/components/ui/PageUpdate.jsx
+++ src/components/ui/PageUpdate.jsx
@@ -1,7 +1,10 @@
+import { X } from "lucide-react";
+
 const PageUpdate = () => {
   return (
-    <div>
-    
+    <div className="flex justify-between gap-x-3 md:gap-x-6 lg:gap-x-4 absolute right-6 bottom-8 md:h-fit md:right-6 md:top-6 lg:right-4 lg:top-4 p-3 md:p-6 lg:p-4 w-64 md:w-2/4 lg:w-96 bg-gray-light md:bg-white rounded-tr-md rounded-bl-md rounded-tl-3xl rounded-br-3xl">
+      <span className="w-full text-red-500 text-[8px] md:text-xl lg:text-base">hello world. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo, possimus?</span>
+      <X className="w-3 h-3 md:w-8 md:h-8 lg:w-5 lg:h-5 cursor-pointer" />
     </div>
   );
 };
