a5a50bf4d99f7b7337ef24ac4d94f12e40c44ef9
Uniquecon <akinwunmiolusegun277@gmail.com>
2025-07-03 12:16:39 +0100

    fix: changed the fucnctionality when toggling the password eye icon

--- src/components/auth/AuthInput.jsx
+++ src/components/auth/AuthInput.jsx
@@ -8,8 +8,7 @@ const AuthInput = ({ type, name, placeholder }) => {
   return (
     <>
       <div className="py-1 px-3 md:px-4 rounded-md md:rounded-xl lg:rounded-lg bg-gray-light flex items-center lg:gap-y-1 text-gray-medium">
-        <Field type={type} name={name} placeholder={placeholder} className="shadow-inset border-none outline-none bg-transparent py-2 px-1 md:py-3 text-xs md:text-xl lg:text-sm w-full" />
-        {/* <input type={type === "password" ? (showPassword ? "text" : "password") : type} autoComplete="on" placeholder={placeholder} name={name} value={value} onChange={onChange} className="shadow-inset border-none outline-none bg-transparent py-2 px-1 md:py-3 text-xs md:text-xl lg:text-sm w-full" /> */}
+        <Field type={type === "password" ? (showPassword ? "text" : "password") : type} name={name} placeholder={placeholder} className="shadow-inset border-none outline-none bg-transparent py-2 px-1 md:py-3 text-xs md:text-xl lg:text-sm w-full" />
         {type === "password" && (showPassword ? <EyeOff onClick={() => setShowPassword(false)} className="w-3 md:w-8 lg:w-5 h-3 md:h-8 lg:h-5 cursor-pointer" /> : <Eye onClick={() => setShowPassword(true)} className="w-3 md:w-8 lg:w-5 h-3 md:h-8 lg:h-5 cursor-pointer" />)}
       </div>
     </>
